6c0a7e435357b879e4b048ab23095b07fa348c15
Laurynas Sakalauskas <laurynas@helix.lt>
2016-02-04 19:29:22 +0000

    Rewrite and optimize

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# conditionize.js
+# Fork of conditionize.js
 A small jQuery plugin for handling showing and hiding things conditionally based on input - typically groups of form fields. It works using data attributes to keep all of the name/values for inputs directly in the markup and saves you the trouble of having to manually show/hide a bunch of stuff through JS, as well as improving maintenance if you need to change the name or value of an input you were listening to.
 
 # Usage
 Conditionize looks for the name of an input to listen to and a value to listen for via data attributes on the element it is initialized on.
 
-To use it, encapsulate form fields you want to show only conditionally in a container with the data attributes data-cond-option and data-cond-value. Give all containers you want conditionize to listen to a class.
+To use it, encapsulate form fields you want to show only conditionally in a container with the data attributes data-cond-option and data-cond-value, data-cond-type, data-option-type. Give all containers you want conditionize to listen to a class.
+
+
 
 ```html
 <label for="foo"><input name="foo" type="checkbox"> Foo</label>
-<div class="conditional" data-cond-option="foo" data-cond-value="on">
+<div class="conditional" data-cond-option="foo" data-cond-value="on" data-cond-option-type="checkbox|select|text|etc" data-cond-type="equals|or|not_equals">
   Bar
 </div>
 ```
@@ -17,14 +19,6 @@ Then, call conditionize on the class...
 
 ```javascript
   $('.conditional').conditionize();
-```
-
-If you'd prefer to use CSS to hide everything that is to be shown conditionally...
-
-```javascript
-  $('.conditional').conditionize({
-    hideJS: false
-  });
 ```
 
 # Demo
--- conditionize.jquery.js
+++ conditionize.jquery.js
@@ -1,61 +1,88 @@
 (function($) {
-    $.fn.conditionize = function(options){
+    $.fn.clearForm = function() {
+        return this.each(function() {
+            var type = this.type, tag = this.tagName.toLowerCase();
+            if (type == 'text' || type == 'password' || tag == 'textarea')
+                this.value = '';
+            else if (type == 'checkbox' || type == 'radio')
+                this.checked = false;
+            else if (tag == 'select')
+                this.selectedIndex = -1;
+        });
+    };
 
-        var settings = $.extend({
-            hideJS: false
-        }, options );
+    $.fn.conditionize = function(){
 
         $.fn.showOrHide = function(listenTo, listenFor, listenType, optionType, $section) {
-            var listenForitems = listenFor.toString().split(',');
 
-            if (optionType == 'radio' || optionType == 'checkbox') {
-                listenTo = listenTo + ':checked';
-            }
+            var listenForItems = listenFor.toString().split(',');
+            var listenToItems = listenTo.toString().split(',');
 
-            if (listenForitems.length > 0) {
-
-                // We have multiple
-
-                if (listenType == 'or') {
-                    // Or
-                    // Check if element is in array
-                    if ($.inArray($(listenTo).val(), listenForitems)) {
-                        $section.slideDown();
+            if (listenType == 'or') {
+                // Or
+                // Check if element is in array
+                var equals = false;
+                listenToItems.forEach(function(listenToItem) {
+                    if ($.inArray($(listenToItem).val(), listenForItems) !== -1) {
+                        equals = true;
                     }
-                    else {
-                        $section.slideUp();
-                    }
+                });
 
-                } else if (listenType == 'equals') {
-                    // Equals
-                    // Check if all satisfy the condition
-                    var equals = false;
-                    listenForitems.forEach(function(entry) {
+                if (equals) {
+                    $section.slideDown();
+                }
+                else {
+                    $section.slideUp();
+                    $section.find(':input').clearForm();
+                }
 
-                        if ($(listenTo).val() == entry) {
+            } else if (listenType == 'equals') {
+                // Equals
+                // Check if all satisfy the condition
+                var equals = true;
+                listenToItems.forEach(function(listenToItem) {
+
+                    listenForItems.forEach(function(entry) {
+
+                        if ($(listenToItem).val() == entry && equals) {
                             equals = true;
                         } else {
                             equals = false;
                         }
                     });
 
-                    if (equals) {
-                        $section.slideDown();
-                    }
-                    else {
-                        $section.slideUp();
-                    }
+                });
 
-                } else {
 
-                    // Not equals
+                if (equals) {
+                    $section.slideDown();
+                }
+                else {
+                    $section.slideUp();
+                    $section.find(':input').clearForm();
+                }
 
-                    if ($.inArray($(listenTo).val(), listenForitems) === -1) {
-                        $section.slideDown();
+            } else {
+
+                // Not equals
+                // If element is in array - hide
+                // Vice versa for OR functionality
+                var equals = true;
+                listenToItems.forEach(function(listenToItem) {
+
+                    if ($.inArray($(listenToItem).val(), listenForItems) !== -1 && equals) {
+                        equals = true;
+                    } else {
+                        equals = false;
                     }
-                    else {
-                        $section.slideUp();
-                    }
+                });
+
+                if (equals) {
+                    $section.find(':input').clearForm();
+                    $section.slideUp();
+                }
+                else {
+                    $section.slideDown();
                 }
 
             }
@@ -64,37 +91,21 @@
 
         return this.each( function() {
             var $section = $(this);
-            var listenTo = "[name='" + $section.data('cond-option') +"']";
-            var optionType = $section.data('cond-option-type');
-            var listenType = $section.data('cond-type');
-            var listenFor = $section.data('cond-value');
-            var selector = '';
+            var listenTo = $section.data('cond-option');
+            var optionType = $section.data('cond-option-type'); // type, selectbox, checkbox, etc..
+            var listenType = $section.data('cond-type'); // or, equals or not_equals
+            var listenFor = $section.data('cond-value'); // the value needed
 
-            switch (optionType) {
-
-                case 'checkbox':
-                case 'text':
-                case 'email':
-                case 'number':
-                case 'textarea':
-                case 'select':
-                case 'radio':
-                    selector = '';
-                    break;
-            }
+            // listen for all changes
+            var listenToALL = listenTo.split(":checked").join("");
 
             //Set up event listener
-            $(listenTo + selector).on('change', function() {
-                $.fn.showOrHide(listenTo + selector, listenFor, listenType, optionType, $section);
+            $(document).on('change load', listenToALL, function() {
+                $.fn.showOrHide(listenTo, listenFor, listenType, optionType, $section);
             });
-            //If setting was chosen, hide everything first...
-            if (settings.hideJS) {
-                $(this).hide();
-            }
-            //Show based on current value on page load
-                $.fn.showOrHide(listenTo + selector, listenFor, listenType, optionType, $section);
+
+            $.fn.showOrHide(listenTo, listenFor, listenType, optionType, $section);
         });
     }
 }(jQuery));
 
-$('.conditional-question').conditionize();
